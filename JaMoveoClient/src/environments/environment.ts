51d3c3c621fc12855b2f41a0b5406f3f168a5dd0
Your Name <38976767+chayaWakshtok@users.noreply.github.com>
2025-09-03 11:12:25 +0300

    fix env

--- JaMoveoClient/src/environments/environment.ts
+++ JaMoveoClient/src/environments/environment.ts
@@ -1,12 +1,12 @@
-// export const environment = {
-//   production: false,
-//   apiUrl: 'https://jamoveoapi20250901224157-aneae9ardag8cdah.canadacentral-01.azurewebsites.net/api/',
-//   signalRUrl: 'https://jamoveoapi20250901224157-aneae9ardag8cdah.canadacentral-01.azurewebsites.net/rehearsalhub'
-// };
-
-
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44352/api/',
-  signalRUrl: 'https://localhost:44352/rehearsalhub'
+  apiUrl: 'https://jamoveoapi20250901224157-aneae9ardag8cdah.canadacentral-01.azurewebsites.net/api/',
+  signalRUrl: 'https://jamoveoapi20250901224157-aneae9ardag8cdah.canadacentral-01.azurewebsites.net/rehearsalhub'
 };
+
+
+// export const environment = {
+//   production: false,
+//   apiUrl: 'https://localhost:44352/api/',
+//   signalRUrl: 'https://localhost:44352/rehearsalhub'
+// };
